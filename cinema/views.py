9d328da515d9047e61fa5566ba19eeff107110b3
Ihor Dmytriv <dmytriwihor@gmail.com>
2025-06-18 17:42:08 +0200

    Add multiple authentication classes to DRF settings

--- cinema/views.py
+++ cinema/views.py
@@ -75,7 +75,6 @@ class MovieViewSet(
 ):
     queryset = Movie.objects.prefetch_related("genres", "actors")
     serializer_class = MovieSerializer
-    authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
 
     @staticmethod
